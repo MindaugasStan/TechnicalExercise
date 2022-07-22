631d4a5f0d809b0d8fca9e01fe461ab6b96bb171
Mindaugas Stanionis <m.stanionis1@gmail.com>
2022-07-22 10:41:58 +0300

    Cleaning code

--- BL/SquareCalculator.cs
+++ BL/SquareCalculator.cs
@@ -92,46 +92,6 @@ namespace BL
                  .ToList();
         }
 
-        private static double distSquare(Point p, Point q)
-        {
-            return Math.Sqrt(Math.Pow(p.XAxis - q.XAxis, 2) + Math.Pow(p.YAxis - q.YAxis, 2));
-
-        }
-        //private static bool isSquare(Point p1, Point p2, Point p3, Point p4)
-        //{
-        //    int d2 = distSquare(p1, p2); // from p1 to p2
-        //    int d3 = distSquare(p1, p3); // from p1 to p3
-        //    int d4 = distSquare(p1, p4); // from p1 to p4
-
-        //    if (d2 == 0 || d3 == 0 || d4 == 0)
-        //        return false;
-
-        //    // If lengths if (p1, p2) and (p1, p3) are same, then
-        //    // following conditions must met to form a square.
-        //    // 1) Square of length of (p1, p4) is same as twice
-        //    // the square of (p1, p2)
-        //    // 2) Square of length of (p2, p3) is same
-        //    // as twice the square of (p2, p4)
-        //    if (d2 == d3 && 2 * d2 == d4
-        //        && 2 * distSquare(p2, p4) == distSquare(p2, p3))
-        //    {
-        //        return true;
-        //    }
-
-        //    // The below two cases are similar to above case
-        //    if (d3 == d4 && 2 * d3 == d2
-        //        && 2 * distSquare(p3, p2) == distSquare(p3, p4))
-        //    {
-        //        return true;
-        //    }
-        //    if (d2 == d4 && 2 * d2 == d3
-        //        && 2 * distSquare(p2, p3) == distSquare(p2, p4))
-        //    {
-        //        return true;
-        //    }
-        //    return false;
-        //}
-
     }
 }
 
